8d5fd0a9f4ed495501cc8d6bfc93d17949f17154
luxixu <luxixu@WSWINCNHZ0656.hengtiansoft.com>
2013-05-06 20:50:02 +0800

    add detail

--- DemoStaticPage/WebContent/js/common.js
+++ DemoStaticPage/WebContent/js/common.js
@@ -1,10 +1,24 @@
 function getWeather() {
-	$.getJSON("http://www.weather.com.cn/data/cityinfo/101211001.html",
-			function(data) {
-				var weatherInfo = data.weatherInfo;
-				var weatherText = weatherInfo.city + "&nbsp;"
-						+ weatherInfo.weather + "&nbsp;" + weatherInfo.temp2
-						+ "~" + weatherInfo.temp1;
-				$(".weather").html(weatherText);
-			});
+
+	var iframe = document.getElementById("weatherFrame");
+	if (iframe.attachEvent) {
+		iframe.attachEvent("onload", function() {
+			// 以下操作必须在iframe加载完后才可进行
+			output();
+		});
+	} else {
+		iframe.onload = function() {
+			// 以下操作必须在iframe加载完后才可进行
+			output();
+		};
+	}
+}
+
+function output() {
+	//var text = $(window.frames["weatherFrame"].document).find("body").html();
+	var text = $("#weatherFrame").find("body").html();
+	var weatherInfo = $.parseJSON(text);
+	var weatherText = weatherInfo.city + "&nbsp;" + weatherInfo.weather
+			+ "&nbsp;" + weatherInfo.temp2 + "~" + weatherInfo.temp1;
+	$(".weather").html(weatherText);
 }
