f3db968d640755b21abec64c724930d6c4163499
aneng <aneng@aneng-PC>
2013-05-09 21:03:22 +0800

    finished job/tour/diner/market page

--- DemoStaticPage/WebContent/js/carousel-mock-data.js
+++ DemoStaticPage/WebContent/js/carousel-mock-data.js
@@ -1,8 +1,8 @@
 var _listdata= [];
 _listdata[0] = {
 	title:'据悉，韩国出品的成人网游《女王之刃》代言人，内地超模艾尚真被选为韩国娱乐杂志《MAXIM（箴言）》的7月刊封面女郎.为了参与《女王之刃》的宣传活动于今年五月访韩的艾尚真，在参与游戏宣传活动之余还参与了《MAXIM》杂志的照片拍摄.',
-	timg:'../images/data/rdn_4fe5f4f8ee668.jpg',
-	img:'../images/data/xes_04115e473f58bf599cbc52794a4c0e3d.jpg', 
+	timg:'../images/data/pick_1.jpg',
+	img:'../images/data/pick_1_1.jpg', 
 	listimg:'../images/data/xes_a6e95a791dcb6a6306fe518dbf3b16b3.jpg',
 	picwidth:'609',
 	picheight:'800',
@@ -10,56 +10,56 @@ _listdata[0] = {
 };
 _listdata[1] = {
 	title:'另一方面，《女王之刃》韩服在前不久进行最终测试，计划年内展开公测.而艾尚真的相关海报除了7月刊《MAXIM》杂志之外，也将通过《女王之刃》的官网公开.',
-	timg:'../images/data/rdn_4fe5f5031b588.jpg',
-	img:'../images/data/xes_c78dc213a6c7bdaf3fd601fd4c24a131.jpg',
+	timg:'../images/data/pick_2.jpg',
+	img:'../images/data/pick_2_1.jpg',
 	listimg:'../images/data/xes_8d6eccfc72e408ddce5a0358fff58be4.jpg',
-	picwidth:'609',
-	picheight:'795',
+	picwidth:'500',
+	picheight:'332',
 	morelink:''
 };
 _listdata[2] = {
 	title:'《MAXIM》杂志女郎',
-	timg:'../images/data/rdn_4fe5f5576aef6.jpg',
-	img:'../images/data/xes_228bfeb5af191a13aedf5ad1abfb9804.jpg',
+	timg:'../images/data/0946085308.jpg',
+	img:'../images/data/0946085308_1.jpg',
 	listimg:'../images/data/xes_73492b4e9e4f0396794ac820d5a575f6.jpg',
-	picwidth:'420',
-	picheight:'548',
+	picwidth:'1843',
+	picheight:'1229',
 	morelink:''
 };
 _listdata[3] = {
 	title:'《MAXIM》杂志女郎',
-	timg:'../images/data/rdn_4fe5f55aaf41b.jpg',
-	img:'../images/data/xes_01a639c9981430cf0b79251ad6674445.jpg',
+	timg:'../images/data/n_1733407360943641.jpg',
+	img:'../images/data/n_1733407360943641_2.jpg',
 	listimg:'../images/data/xes_cb42378f915f2f76efe3587723ec397b.jpg',
-	picwidth:'420',
-	picheight:'705',
+	picwidth:'640',
+	picheight:'430',
 	morelink:''
 };
 _listdata[4] = {
 	title:'《MAXIM》杂志女郎',
-	timg:'../images/data/rdn_4fe5f5645fccb.jpg',
-	img:'../images/data/xes_677a1fa59ca7d8ed6eb4bdb638827dbe.jpg',
+	timg:'../images/data/n_1688431845262872.jpg',
+	img:'../images/data/n_1688431845262872_1.jpg',
 	listimg:'../images/data/xes_abef0d5b72113a5c3c3198a18818a97a.jpg',
-	picwidth:'420',
-	picheight:'588',
+	picwidth:'640',
+	picheight:'426',
 	morelink:''
 };
 _listdata[5] = {
 	title:'《MAXIM》杂志女郎',
-	timg:'../images/data/rdn_4fe5f56759e7b.jpg',
-	img:'../images/data/xes_aaf07ee19b87832c38027fcd836a41bf.jpg',
+	timg:'../images/data/200813144842134_2.jpg',
+	img:'../images/data/200813144842134_1.jpg',
 	listimg:'../images/data/xes_73d9a467941109b2123b70dec0d2d716.jpg',
-	picwidth:'420',
-	picheight:'523',
+	picwidth:'1024',
+	picheight:'730',
 	morelink:''
 };
 _listdata[6] = {
 	title:'《MAXIM》杂志女郎',
-	timg:'../images/data/rdn_4fe5f56ad5a52.jpg',
-	img:'../images/data/xes_2399f1439a02d3dfe021f9b560550dfa.jpg',
+	timg:'../images/data/200911121258028882.jpg',
+	img:'../images/data/200911121258028882_1.jpg',
 	listimg:'../images/data/xes_6b554e8ba7565eecd387d59d38270f81.jpg',
-	picwidth:'420',
-	picheight:'630',
+	picwidth:'600',
+	picheight:'450',
 	morelink:''
 };
 new ifeng.Gallery({
